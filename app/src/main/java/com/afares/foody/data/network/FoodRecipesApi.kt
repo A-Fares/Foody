6cc7dc539b9481f04d52b828a0aba58469a749eb
AFares1 <ahmedfaresa10@gmail.com>
2021-01-31 01:51:46 +0200

    Design RecipesFragment Search Menu and set search recipes fun in FoodRecipesApi

--- app/src/main/java/com/afares/foody/data/network/FoodRecipesApi.kt
+++ app/src/main/java/com/afares/foody/data/network/FoodRecipesApi.kt
@@ -11,4 +11,10 @@ interface FoodRecipesApi {
     suspend fun getRecipes(
             @QueryMap queries: Map<String, String>
     ): Response<FoodRecipe>
+
+    @GET("/recipes/complexSearch")
+    suspend fun searchRecipes(
+            @QueryMap searchQuery: Map<String, String>
+    ): Response<FoodRecipe>
+
 }
--- app/src/main/java/com/afares/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/afares/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -2,10 +2,9 @@ package com.afares.foody.ui.fragments.recipes
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -27,7 +26,7 @@ import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
-class RecipesFragment : Fragment() {
+class RecipesFragment : Fragment(), SearchView.OnQueryTextListener {
 
     private val args by navArgs<RecipesFragmentArgs>()
 
@@ -92,6 +91,23 @@ class RecipesFragment : Fragment() {
         showShimmerEffect()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.recipes_menu, menu)
+
+        val search = menu.findItem(R.id.menu_search)
+        val searchView = search.actionView as? SearchView
+        searchView?.isSubmitButtonEnabled = true
+        searchView?.setOnQueryTextListener(this)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+    }
+
     private fun readDatabase() {
         lifecycleScope.launch {
             mainViewModel.readRecipe.observeOnce(viewLifecycleOwner, { database ->
@@ -154,6 +170,7 @@ class RecipesFragment : Fragment() {
         super.onDestroy()
         _binding = null
     }
+
 
 }
 
