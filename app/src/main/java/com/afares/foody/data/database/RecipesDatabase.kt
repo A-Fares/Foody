5251773342dfa924694190bf530bf7cfb375fb41
AFares1 <ahmedfaresa10@gmail.com>
2021-02-03 23:56:21 +0200

    Modify RecipesDatabase class

--- app/src/main/java/com/afares/foody/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/afares/foody/data/database/RecipesDatabase.kt
@@ -3,12 +3,13 @@ package com.afares.foody.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.afares.foody.data.database.entities.FavoritesEntity
 import com.afares.foody.data.database.entities.RecipesEntity
 
 @Database(
-        entities = [RecipesEntity::class],
-        version = 1,
-        exportSchema = false
+    entities = [RecipesEntity::class, FavoritesEntity::class],
+    version = 1,
+    exportSchema = false
 )
 @TypeConverters(RecipesTypeConverter::class)
 abstract class RecipesDatabase: RoomDatabase() {
