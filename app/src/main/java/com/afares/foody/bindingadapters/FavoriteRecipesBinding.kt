0f02fa20300bdb4d9fa59fb6a8d984cae09c0e70
AFares1 <ahmedfaresa10@gmail.com>
2021-02-06 11:57:57 +0200

    Display Favorite Recipes and Create FavoriteRecipesBinding class

---
+++ app/src/main/java/com/afares/foody/bindingadapters/FavoriteRecipesBinding.kt
@@ -0,0 +1,74 @@
+package com.afares.foody.bindingadapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.afares.foody.adapters.FavoriteRecipesAdapter
+import com.afares.foody.data.database.entities.FavoritesEntity
+
+class FavoriteRecipesBinding {
+   /* companion object {
+
+        @BindingAdapter("viewVisibility", "setData", requireAll = false)
+        @JvmStatic
+        fun setDataAndViewVisibility(
+            view: View,
+            favoritesEntity: List<FavoritesEntity>?,
+            mAdapter: FavoriteRecipesAdapter?
+        ) {
+            if (favoritesEntity.isNullOrEmpty()) {
+                when (view) {
+                    is ImageView -> {
+                        view.visibility = View.VISIBLE
+                    }
+                    is TextView -> {
+                        view.visibility = View.VISIBLE
+                    }
+                    is RecyclerView -> {
+                        view.visibility = View.INVISIBLE
+                    }
+                }
+            } else {
+                when (view) {
+                    is ImageView -> {
+                        view.visibility = View.INVISIBLE
+                    }
+                    is TextView -> {
+                        view.visibility = View.INVISIBLE
+                    }
+                    is RecyclerView -> {
+                        view.visibility = View.VISIBLE
+                        mAdapter?.setData(favoritesEntity)
+                    }
+                }
+            }
+        }
+
+    }*/
+    companion object {
+
+        @BindingAdapter("setVisibility", "setData", requireAll = false)
+        @JvmStatic
+        fun setVisibility(
+            view: View,
+            favoritesEntity: List<FavoritesEntity>?,
+            mAdapter: FavoriteRecipesAdapter?
+        ) {
+            when (view) {
+                is RecyclerView -> {
+                    val dataCheck = favoritesEntity.isNullOrEmpty()
+                    view.isInvisible = dataCheck
+                    if (!dataCheck) {
+                        favoritesEntity?.let { mAdapter?.setData(it) }
+                    }
+                }
+                else -> view.isVisible = favoritesEntity.isNullOrEmpty()
+            }
+        }
+
+    }
+}
