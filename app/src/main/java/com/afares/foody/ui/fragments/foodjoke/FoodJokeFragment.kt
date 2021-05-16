fe9e2e9bf9125f3e6b2261486c6afa356022217c
AFares1 <ahmedfaresa10@gmail.com>
2021-05-17 00:26:47 +0200

    FoodJokeFragment setup

--- app/src/main/java/com/afares/foody/ui/fragments/foodjoke/FoodJokeFragment.kt
+++ app/src/main/java/com/afares/foody/ui/fragments/foodjoke/FoodJokeFragment.kt
@@ -1,21 +1,75 @@
 package com.afares.foody.ui.fragments.foodjoke
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.afares.foody.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.afares.foody.databinding.FragmentFoodJokeBinding
+import com.afares.foody.util.Constants.Companion.API_KEY
+import com.afares.foody.util.NetworkResult
+import com.afares.foody.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class FoodJokeFragment : Fragment() {
+
+    private val mainViewModel by viewModels<MainViewModel>()
+
+    private var _binding: FragmentFoodJokeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_joke, container, false)
+        _binding = FragmentFoodJokeBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.mainViewModel = mainViewModel
+
+
+
+        mainViewModel.getFoodJoke(API_KEY)
+        mainViewModel.foodJokeResponse.observe(viewLifecycleOwner, { response ->
+            when (response) {
+                is NetworkResult.Success -> {
+                    binding.foodJokeTextView.text = response.data?.text
+                }
+                is NetworkResult.Error -> {
+                    loadDataFromCache()
+                    Toast.makeText(
+                        requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is NetworkResult.Loading -> {
+                    Log.d("FoodJokeFragment", "Loading")
+                }
+            }
+        })
+
+        return binding.root
     }
 
+    private fun loadDataFromCache() {
+        lifecycleScope.launch {
+            mainViewModel.readFoodJoke.observe(viewLifecycleOwner, { database ->
+                if (!database.isNullOrEmpty()) {
+                    binding.foodJokeTextView.text = database[0].foodJoke.text
+                }
+            })
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
